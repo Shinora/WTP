c55ffccf7fce13742a7a4854f0c852ab30906d55
Shuiza <25851188+Shinora@users.noreply.github.com>
2018-12-08 15:41:46 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,20 @@
-readme = open('README.md').read()
-VERSION = "0.0.3"
+readme= open('README.md').read()
+VERSION= "0.0.3"
 
-requirements = []
+requirements= []
 
 setup(
-    
-    name = 'Web Transfert Protocol',
-    version = VERSION,
-    author = 'Torzivalds',
-    author_email = 'tozivalds@gmail.com',
-    url = 'https://myrasp.fr/WTP',
-    description ='P2P Network',
-    long_description = readme,
-    license = 'GPL3.0',
-
+    name= 'Web Transfert Protocol',
+    version= VERSION,
+    author= 'Torzivalds',
+    author_email= 'tozivalds@gmail.com',
+    url= 'https://myrasp.fr/WTP',
+    description='P2P Network',
+    long_description= readme,
+    license= 'GPL3.0'
     # Package info
-    packages = find_packages(exclude=('test',)),
-    zip_safe = True,
-    install_requires = requirements,
-    extras_require = {},
+    packages= find_packages(exclude=('test',)),
+    zip_safe= True,
+    install_requires= requirements,
+    extras_require= {},
 )
