b93917d25b254d9a9dd3a2c071f4d69d9552ffb2
Torzivalds <torzivalds@gmail.com>
2019-01-01 14:02:52 +0000

    The search or files has been improved

--- search.py
+++ search.py
@@ -21,13 +21,13 @@ def searchFile(nomFichier):
 		# sont des noeuds "simple", puis mettre les noeuds "simple" au début du tableau
 		tableauNoeudsSimple = []
 		tableauSuperNoeuds = []
-		for x in range(len(tblNoeuds)):
+		for noeud in tblNoeuds:
 			# On verifie chaque noeud
-			fonctionNoeud = BDD.chercherInfo("Noeuds", tblNoeuds[x], "Fonction")
+			fonctionNoeud = BDD.chercherInfo("Noeuds", noeud, "Fonction")
 			if fonctionNoeud == "simple":
-				tableauNoeudsSimple.append(tblNoeuds[x])
+				tableauNoeudsSimple.append(noeud)
 			if fonctionNoeud == "supernoeud":
-				tableauSuperNoeuds.append(tblNoeuds[x])
+				tableauSuperNoeuds.append(noeud)
 		if len(tableauNoeudsSimple) < 6:
 			# Il n'y a pas assez de noeuds simples dans la liste,
 			# Il faut aller en rechercher
@@ -51,7 +51,7 @@ def searchFile(nomFichier):
 			if msg_recu != "0":
 				IPPortNoeud = msg_recu
 				break
-	return IPPortNoeud
+	return IPPortNoeud # Peut retourner "" si il ne trouve pas d'hébergeur
 
 def chercherFichier(nomFichier):
 	# Fonction qui regarde dans sa BDD si il y a le fichier en question
@@ -66,9 +66,56 @@ def chercherFichier(nomFichier):
 		# ATTENTION ! Si le fichier n'est pas connu, 0 est retourné
 		return retour
 
-def searchDNS(url):
+def searchNDD(url):
 	# Fonction qui vérifie si le nom de fichier envoyé est plus petit qu'un SHA256
 	# Si c'est le cas, il va chercher chez les noeuds de DNS si ils connaissent ce nom de domaine
+	# Renvoie un SHA256
+	sha = ""
 	if url < 64:
 		# Ce n'est pas un nom de fichier, car il est plus petit qu'un SHA256
-		
+		tableau = BDD.searchNoeud("DNS", 25)
+		for noeud in tableau:
+			ip = noeud[:noeud.find(":")]
+			port = noeud[noeud.find(":")+1:]
+			connexion_avec_serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			connexion_avec_serveur.connect((ip, port))
+			msg_a_envoyer = b""
+			msg_a_envoyer = "=cmd DNS searchSHA ndd " + url
+			msg_a_envoyer = msg_a_envoyer.encode()
+			connexion_avec_serveur.send(msg_a_envoyer)
+			msg_recu = connexion_avec_serveur.recv(1024)
+			msg_recu = msg_recu.decode()
+			connexion_avec_serveur.close()
+			if msg_recu != "=cmd NNDInconnu" and msg_recu != "=cmd ERROR":
+				# On a trouvé !!
+				sha = msg_recu
+				break
+	return sha
+
+def rechercheFichierEntiere(donnee):
+	# Fonction qui cherche jusqu'à trouver l'IpPort d'un noeud qui héberge le fichier
+	# La donnée passée en paramètres peut etre un nom de domaine, ou un sha256
+	retour = ""
+	if len(str(donnee)) <= 63:
+		# C'est un nom de domaine
+		retour = ""
+		nbre = 0
+		while retour == "":
+			nbre += 1
+			retour = searchNDD(donnee)
+			if retour == "" and nbre > 9:
+				# La fonction n'a pas trouvé le sha256 associé à ce nom de domaine
+				# Elle a pu demander jusqu'à 250 DNS différents
+				return "=cmd BADDNS"
+	# Si on arrive là c'est que l'on connait le sha256 du fichier (c'est retour)
+	retour2 = str(chercherFichier(retour))
+	if retour2 != "0":
+		# Le noeud héberge déjà le fichier ou le connait
+		return retour2
+	else:
+		# Le noeud ne connait pas le fichier
+		retour3 = searchFile(retour)
+		if retour3 != "":
+			return retour3
+		else:
+			return "=cmd NOHOST"
